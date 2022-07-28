7bacb09a4148cdc8e2492ab9a98472c6493ba03d
Robin Thöne <>
2022-07-28 23:40:29 +0200

    use the link of the new github profile

--- app/components/menus/TopMenu.tsx
+++ app/components/menus/TopMenu.tsx
@@ -12,7 +12,7 @@ import { LanguageSelector } from '../LanguageSelector';
 export const TopMenu: FunctionComponent = () => {
     return (
         <div className="fixed top-0 left-0 flex w-full py-4 px-8 items-center bg-gray-50 xl:bg-transparent z-30">
-            <a aria-label="GitHub" href="https://github.com/IR0B1NI" target="_blank" rel="noreferrer">
+            <a aria-label="GitHub" href="https://github.com/robin-thoene" target="_blank" rel="noreferrer">
                 <FontAwesomeIcon icon={faGithub} size="lg" className="h-5 w-5" />
             </a>
             <a aria-label="LinkedIn" className="ml-4" href="https://linkedin.com/in/robin-thöne-681870205" target="_blank" rel="noreferrer">
