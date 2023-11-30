bb1bc1817f5cfe4cd23e1b686404b922dbab31e8
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2023-11-30 23:40:57 +0100

    fix layout for large screens

--- src/app.rs
+++ src/app.rs
@@ -37,55 +37,53 @@ fn Home() -> impl IntoView {
     view! {
         <Title text="Robin Thöne"/>
         <Html class="dark"/>
-        <main>
-            <div class="max-w-screen-2xl">
-                <div class="w-full flex justify-between fixed bg-white dark:bg-black top-0 left-0 p-6">
-                    <div class="flex flex-row">
-                        <a href="https://github.com/robin-thoene" target="_blank" rel="noreferrer">
-                            <Icon class="h-5 w-5" icon=Icon::from(FaIcon::FaGithubBrands)/>
-                        </a>
-                        <a
-                            class="ml-4"
-                            href="https://linkedin.com/in/robin-thöne-681870205"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <Icon class="h-5 w-5" icon=Icon::from(FaIcon::FaLinkedinBrands)/>
-                        </a>
-                        <a
-                            class="ml-4"
-                            href="https://www.xing.com/profile/Robin_Thoene"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <Icon class="h-5 w-5" icon=Icon::from(FaIcon::FaXingBrands)/>
-                        </a>
-                    </div>
-                    <div>
-                        <button
-                            class="mr-4"
-                            class=("text-primary", move || i18n.get_locale() == Locale::en)
-                            on:click=set_locale_english
-                        >
-                            [en]
-                        </button>
-                        <button
-                            class=("text-primary", move || i18n.get_locale() == Locale::de)
-                            on:click=set_locale_german
-                        >
-                            [de]
-                        </button>
-                    </div>
+        <main class="w-screen max-w-screen-2xl p-6">
+            <div class="max-w-screen-2xl fixed inset-x-0 mx-auto w-full flex justify-between bg-white dark:bg-black top-0 left-0 p-6">
+                <div class="flex flex-row">
+                    <a href="https://github.com/robin-thoene" target="_blank" rel="noreferrer">
+                        <Icon class="h-5 w-5" icon=Icon::from(FaIcon::FaGithubBrands)/>
+                    </a>
+                    <a
+                        class="ml-4"
+                        href="https://linkedin.com/in/robin-thöne-681870205"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Icon class="h-5 w-5" icon=Icon::from(FaIcon::FaLinkedinBrands)/>
+                    </a>
+                    <a
+                        class="ml-4"
+                        href="https://www.xing.com/profile/Robin_Thoene"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <Icon class="h-5 w-5" icon=Icon::from(FaIcon::FaXingBrands)/>
+                    </a>
                 </div>
-                <h1>Robin Thöne</h1>
-                <p>TODO: Profile</p>
-                <h2>{t!(i18n, education_headline)}</h2>
-                <p>TODO: Education timeline</p>
-                <h2>{t!(i18n, work_experience_headline)}</h2>
-                <p>TODO: work experience timeline</p>
-                <h2>{t!(i18n, side_projects_headline)}</h2>
-                <p>TODO: projects</p>
+                <div>
+                    <button
+                        class="mr-4"
+                        class=("text-primary", move || i18n.get_locale() == Locale::en)
+                        on:click=set_locale_english
+                    >
+                        [en]
+                    </button>
+                    <button
+                        class=("text-primary", move || i18n.get_locale() == Locale::de)
+                        on:click=set_locale_german
+                    >
+                        [de]
+                    </button>
+                </div>
             </div>
+            <h1>Robin Thöne</h1>
+            <p>TODO: Profile</p>
+            <h2>{t!(i18n, education_headline)}</h2>
+            <p>TODO: Education timeline</p>
+            <h2>{t!(i18n, work_experience_headline)}</h2>
+            <p>TODO: work experience timeline</p>
+            <h2>{t!(i18n, side_projects_headline)}</h2>
+            <p>TODO: projects</p>
         </main>
     }
 }
