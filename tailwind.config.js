491c8bb98087def662d0c4b60d761468a7541f48
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2023-11-30 23:21:39 +0100

    add indicator for current language

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html", "./src/**/*.rs",],
+  content: ["*.html", "./src/**/*.rs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#bd93f9",
+      },
+    },
   },
   plugins: [],
-}
-
+};
